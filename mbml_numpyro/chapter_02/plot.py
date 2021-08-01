5d44dcdf80f3d044d2c267dc029f93ef2937beee
bdatko <ben.datko@gmail.com>
2021-08-01 12:22:28 -0400

    docs(mbml_numpyro/chapter_02/plot.py): add docs for DisplaySkill class and `plot` method

--- mbml_numpyro/chapter_02/plot.py
+++ mbml_numpyro/chapter_02/plot.py
@@ -13,6 +13,21 @@ from dataclasses import dataclass, field
 
 @dataclass
 class DisplaySkill:
+    """
+    Plots Figure 2.15 from `response` array, where incorrect answers
+    are colored with the relevant skill needed to answer the question.
+    Will overlay, at most, two skills onto a single question.
+
+    :param cmap ListedColormap: Colormap object generated from a list of colors.
+    :param skills_key List[List[int]]: Sparse list of length `n_questions`, where
+        each value is the list of required skills for question n
+    :param color_skills: Dict[str, int]: mapping between the skill needed for question
+        and index of `cmap`. Used repalce values of `respones` array
+    :param figsize Tuple[int, int]: size of the figure, passed to `matplotlib.pyplot.subplots`
+    :param copy Bool: Either modify the `response` array inplace when plotting or not
+    :param return_fig_ax Bool: Either return the `fig` and `ax` or not
+    """
+
     cmap: ListedColormap
     skills_key: List[List[int]]
     color_skills: Dict
@@ -20,7 +35,7 @@ class DisplaySkill:
     rectangles: List[Rectangle] = field(
         default_factory=list, compare=False, hash=False, repr=False
     )
-    figsize: tuple[int, int] = (20, 10)
+    figsize: Tuple[int, int] = (20, 10)
     copy: bool = True
     return_fig: bool = False
 
@@ -54,6 +69,16 @@ class DisplaySkill:
         return answers
 
     def plot(self, responses: pd.DataFrame):
+        """
+        Plot the Figure 2.15 from `responses`.
+
+        :param responses pd.DataFrame: DataFrame of the array of the correct and incorrect
+            responses.
+            Except `responses.shape` == (n_questions, n_participants)
+            Except `responses.dtype` == int
+            Except `respones.columns` == range(n_participants)
+            Except `respones.index` == range(n_questions)
+        """
 
         if self.rectangles:
             self.rectangles.clear()
