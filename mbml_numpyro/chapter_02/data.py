19cd685c2f999aa17953228af65940060c31fda5
bdatko <ben.datko@gmail.com>
2021-08-07 15:30:50 -0400

    feat(mbml_numpyro/chapter_02/data.py): data from chapter 02 is now a dataclass instead of a dict
    dataclass allows for documenting properties of the data
    
    n

--- mbml_numpyro/chapter_02/data.py
+++ mbml_numpyro/chapter_02/data.py
@@ -1,7 +1,45 @@
 """
 Data for Chapter 2
 """
+from dataclasses import dataclass, field
+from typing import List
+
 import pandas as pd
+
+
+@dataclass(frozen=True, order=True)
+class ChapterData02:
+    """
+    DataClass for Chapter 02 data
+
+    :param raw_data pd.DataFrame: raw data
+        `raw_data.shape`: (23,56)
+            columns[0]: indicator, dtype == object
+            columns[1:8]: self assessed skills from each participants, dtype == object
+            columns[8:]: respones for each each question, dtype == int64
+        `raw_data.iloc[0,8:]` answers to the test
+    :param self_assessed pd.DataFrame: self assessed skills from each participants
+        `self_assessed` == `raw_data.iloc[1:, 1:8]`
+        `self_assessed.shape`: (22,7)
+            columns: self assessed skills from each participants, dtype == bool
+    :param skills_key pd.DataFrame: skills nesscary for each question as boolean indicator, where columns are the skills and rows are the test questions
+        `skils_key.shape`: (48,7)
+            columns: n_skills
+            rows: n_questions
+    :params skills_needed List[List[int]]: sparse version of skills_key
+    :params responses pd.DataFrame: Graded responses, where columns are participants and rows are their grades prespones
+        `responses.dtypes`: int32, because of ploting
+        `responses.shape`: (48,22)
+            columns: n_participants
+            rows: n_questions
+    """
+
+    raw_data: pd.DataFrame
+    self_assessed: pd.DataFrame
+    skills_key: pd.DataFrame
+    skills_needed: List[List[int]]
+    responses: pd.DataFrame
+
 
 # data code is form the post from the pyro forum
 # https://forum.pyro.ai/t/model-based-machine-learning-book-chapter-2-skills-example-in-pyro-tensor-dimension-issue/464/3?u=bdatko
@@ -10,7 +48,9 @@ import pandas as pd
 raw_data = pd.read_csv(
     "http://www.mbmlbook.com/Downloads/LearningSkills_Real_Data_Experiments-Original-Inputs-RawResponsesAsDictionary.csv"
 )
-self_assessed = raw_data.iloc[1:, 1:8]
+self_assessed = raw_data.iloc[1:, 1:8].copy()
+self_assessed = self_assessed.astype(bool)
+
 skills_key = pd.read_csv(
     "http://www.mbmlbook.com/Downloads/LearningSkills_Real_Data_Experiments-Original-Inputs-Quiz-SkillsQuestionsMask.csv",
     header=None,
@@ -25,10 +65,8 @@ responses = pd.read_csv(
     header=None,
 )
 
-chapter_02_data = {
-    "raw_data": raw_data,
-    "self_assessed": self_assessed,
-    "skills_key": skills_key,
-    "skills_needed": skills_needed,
-    "responses": responses,
-}
+responses = responses.astype("int32")
+
+chapter_02_data = ChapterData02(
+    raw_data, self_assessed, skills_key, skills_needed, responses
+)
